64eb50e59b20c7d592a1113c7f8733f12fcd18a8
kimhaech <epiclog@hufs.ac.kr>
2021-08-02 18:06:48 +0900

    event listener

--- JavaScript/momentum/app.js
+++ JavaScript/momentum/app.js
@@ -1,8 +1,38 @@
-const title = document.querySelector('.hello h1')
+const h1 = document.querySelector("div.hello h1")
 
-function handleTitleClick() {
-  title.style.color = 'blue'
-  console.log('title was clicked!')
+function handleh1Click(){
+  h1.style.color = "blue"
 }
 
-title.addEventListener('click', handleTitleClick)
+function handleMouseEnter(){
+  h1.innerText = "Mouse is here"
+}
+
+function handleMouseLeave(){
+  h1.innerText = "Mouse is gone"
+}
+
+function handleWindowResize(){
+  document.body.style.backgroundColor = "green"
+}
+
+function handleCopy(){
+  alert("copier!!!")
+}
+
+function handleOffline(){
+  alert("NO WIFI...")
+}
+
+function handleOnline(){
+  alert("WIFI YES!")
+}
+
+h1.addEventListener("click",handleh1Click)
+h1.addEventListener("mouseenter",handleMouseEnter)
+h1.addEventListener("mouseleave",handleMouseLeave)
+
+window.addEventListener("resize",handleWindowResize)
+window.addEventListener("copy",handleCopy)
+window.addEventListener("offline",handleOffline)
+window.addEventListener("online",handleOnline)
