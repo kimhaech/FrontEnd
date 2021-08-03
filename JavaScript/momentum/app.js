c1054c038357398f592d31e3553712c12c26616b
kimhaech <epiclog@hufs.ac.kr>
2021-08-03 12:57:56 +0900

    css,class name

--- JavaScript/momentum/app.js
+++ JavaScript/momentum/app.js
@@ -1,38 +1,8 @@
 const h1 = document.querySelector("div.hello h1")
 
 function handleh1Click(){
-  h1.style.color = "blue"
-}
-
-function handleMouseEnter(){
-  h1.innerText = "Mouse is here"
-}
-
-function handleMouseLeave(){
-  h1.innerText = "Mouse is gone"
-}
-
-function handleWindowResize(){
-  document.body.style.backgroundColor = "green"
-}
-
-function handleCopy(){
-  alert("copier!!!")
-}
-
-function handleOffline(){
-  alert("NO WIFI...")
-}
-
-function handleOnline(){
-  alert("WIFI YES!")
+  const clickedClass = "active"
+  h1.classList.toggle(clickedClass)
 }
 
 h1.addEventListener("click",handleh1Click)
-h1.addEventListener("mouseenter",handleMouseEnter)
-h1.addEventListener("mouseleave",handleMouseLeave)
-
-window.addEventListener("resize",handleWindowResize)
-window.addEventListener("copy",handleCopy)
-window.addEventListener("offline",handleOffline)
-window.addEventListener("online",handleOnline)
