cf4d7c6695c3a7431281dccb8dd3b1b19e60343c
kimhaech <epiclog@hufs.ac.kr>
2021-08-05 16:06:35 +0900

    save todos

--- JavaScript/application/js/todo.js
+++ JavaScript/application/js/todo.js
@@ -2,7 +2,15 @@ const toDoForm = document.getElementById("todo-form")
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.getElementById("todo-list")
 
-function deleteToDO(event){
+const TODOS_KEY = "todos"
+
+const toDos = []
+
+function saveToDos(){ // local storage에 저장하는 함수
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+}
+
+function deleteToDO(event){ // todo 삭제 함수
   const li = event.target.parentElement
   li.remove()
 }
@@ -19,12 +27,26 @@ function paintToDo(newTodo){
   toDoList.appendChild(li)
 }
 
+
+
 function handleToDoSubmit(event){
   event.preventDefault()
   console.log(toDoInput.value)
   const newTodo = toDoInput.value // 값을 복사
   toDoInput.value = ""  // 엔터를 치고 값을 비운다.
-  paintToDo(newTodo)
+  toDos.push(newTodo) // toDos에 push
+  paintToDo(newTodo)  // todo를 화면에 보여주는 함수
+  saveToDos() // todo 저장하는 함수 호출
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit)
+
+function sayHello(item){
+  console.log("this is the turn of", item)
+}
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if(savedToDos!==null){
+  const parsedToDos = JSON.parse(savedToDos)
+  parsedToDos.forEach(sayHello);
+}
