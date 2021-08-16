aa8b5bf3395492171185d401f2fbd78ae2c9c287
kimhaech <epiclog@hufs.ac.kr>
2021-08-16 23:11:01 +0900

    react

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                Hey~ React!!!
+            </div>
+        );
+    }
+}
+
+export default App;
