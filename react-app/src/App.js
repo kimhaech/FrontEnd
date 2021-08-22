a42a41f509ce5dab9392030c4798d788a42d2daa
kimhaech <epiclog@hufs.ac.kr>
2021-08-22 15:42:38 +0900

    component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,55 @@
-import logo from './logo.svg';
 import React, {Component} from 'react';
 import './App.css';
 
-class App extends Component {
+class Subject extends Component {
     render() {
         return (
-            <div className="App">
-                Hey~ React!!!
-            </div>
+            <header>
+                <h1>WEB</h1>
+                world wide web!
+            </header>
         );
+    }
+}
+
+class Mid extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>
+                        <a href="1.html">HTML</a>
+                    </li>
+                    <li>
+                        <a href="2.html">CSS</a>
+                    </li>
+                    <li>
+                        <a href="3.html">JavaScript</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+class Bot extends Component {
+    render() {
+        return (
+            <article>
+                <h2>HTML</h2>
+                HTML is HyperText Markup Language.
+            </article>
+        );
+    }
+}
+
+class App extends Component {
+    render() {
+        return (<div className="App">
+            <Subject></Subject>
+            <Mid></Mid>
+            <Bot></Bot>
+        </div>);
     }
 }
 
