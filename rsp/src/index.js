c122a30c81be1e5fb95f65358e621c31ca45c1e4
kimhaech <epiclog@hufs.ac.kr>
2021-08-25 16:58:41 +0900

    add rsp

--- rsp/src/index.js
+++ rsp/src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let count = 0;
+let me = '';
+let other = '';
+let result = 'ql ';
+
+const w_case = {
+  r : 's',
+  s : 'p',
+  p : 'r',
+};
+
+function getResult(me,other){
+  if(count !== 0 && count%2 === 0){
+    if(w_case[me] === other) 
+      result = '승리';
+    else if(w_case[other]=== me) 
+      result =  '패배';
+    else 
+      result = '무승부';
+    console.log(result)
+  }
+}
+
+function handleClick1(){
+  console.log("가위바위보!");
+  if(count%2 === 0){ // count가 2의 배수->me
+    me = 's'
+  }
+  else{
+    other = 's'
+  }
+  count++;
+  getResult(me,other)
+}
+function handleClick2(){
+  console.log("가위바위보!");
+  if(count%2 === 0){ // count가 2의 배수->me
+    me = 'r'
+  }
+  else{
+    other = 'r'
+  }
+  count++;
+  getResult(me,other)
+}
+function handleClick3(){
+  console.log("가위바위보!");
+  if(count%2 === 0){ // count가 2의 배수->me
+    me = 'p'
+  }
+  else{
+    other = 'p'
+  }
+  count++;
+  getResult(me,other)
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <>
+        <h1 id="title">가위바위보</h1>
+        <h2>{result}</h2>
+        <button onClick={handleClick1}>가위</button>
+        <button onClick={handleClick2}>바위</button>
+        <button onClick={handleClick3}>보</button>
+    </>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
