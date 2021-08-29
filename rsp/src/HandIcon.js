b140da03e5a484e45433ec498f2fca18b206adcf
kimhaech <epiclog@hufs.ac.kr>
2021-08-29 16:24:59 +0900

    add

--- rsp/src/HandIcon.js
+++ rsp/src/HandIcon.js
@@ -1,6 +1,16 @@
-const 
-function HandIcon() {
-    return 
+import img_rock from './assets/rock.svg'
+import img_scissor from './assets/scissor.svg'
+import img_paper from './assets/paper.svg'
+
+const rspIcon = {
+    rock : img_rock,
+    scissor : img_scissor,
+    paper : img_paper,
+}
+function HandIcon({value}) {
+    const src = rspIcon[value]
+    const alt = value
+    return <img src={src} alt={alt} />
 }
 
 export default HandIcon
--- rsp/src/index.js
+++ rsp/src/index.js
@@ -1,48 +1,7 @@
 import ReactDOM from 'react-dom';
-
-let count = 0;
-let me = '';
-let other = '';
-let result = '';
-
-const w_case = {
-  r : 's',
-  s : 'p',
-  p : 'r',
-};
-
-function getResult(me,other){
-  if(count !== 0 && count%2 === 0){
-    if(w_case[me] === other) 
-      result = '승리';
-    else if(w_case[other]=== me) 
-      result =  '패배';
-    else 
-      result = '무승부';
-    console.log(result)
-  }
-}
-
-function handleClick(hand){
-  console.log("가위바위보!");
-  if(count%2 === 0){ // count가 2의 배수->me
-    me = hand
-  }
-  else{
-    other = hand
-  }
-  count++;
-  getResult(me,other)
-}
-
+import App from './App';
 
 ReactDOM.render(
-    <>
-        <h1 id="title">가위바위보</h1>
-        <h2>{result}</h2>
-        <button onClick={handleClick('s')}>가위</button>
-        <button onClick={handleClick('r')}>바위</button>
-        <button onClick={handleClick('p')}>보</button>
-    </>,
+    <App />,
     document.getElementById('root')
 );
