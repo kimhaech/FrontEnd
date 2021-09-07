d660e6d7aeb55b5bc74c9b43c3fb54f9e21f9772
kimhaech <epiclog@hufs.ac.kr>
2021-09-07 18:05:52 +0900

    add dice, rsp gameHistory

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -8,15 +8,21 @@ function random(n) {
 }
 
 function App() {
-  const [num, setNum] = useState(1)
+  const [num, setNum] = useState(1) // 주사위 눈 값을 위한 state
+  const [sum, setSum] = useState(0) // 점수 합산을 위한 state
+  const [gameHistory, setGameHistory] = useState([])  // 점수 기록을 위한 배열 state
 
   const handleRollClick = () => {
     const nextNum = random(6) //1~6까지의 랜덤한 숫자를 가진다.
     setNum(nextNum)
+    setSum(sum + nextNum) // 주사위 눈 값 더하기
+    setGameHistory([...gameHistory, nextNum]) // 값 설정 
   }
 
   const handleClearClick = () => {
     setNum(1)
+    setSum(0) // 합 0으로 초기화
+    setGameHistory([])  // 빈 배열 전달
   }
 
   return (
@@ -25,7 +31,14 @@ function App() {
         <Button onClick={handleRollClick}>던지기</Button>
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="red" num={num} />
+      <div>
+        <h2>나</h2>
+        <Dice color="red" num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <p>기록</p>
+        <p>{gameHistory.join(', ')}</p>
+      </div>
     </div>
   )
 }
--- rsp/src/App.js
+++ rsp/src/App.js
@@ -18,23 +18,33 @@ function random(n) {
 function App() {
   const [Hand, setHand] = useState('rock')
   const [OtherHand, setOtherHand] = useState('rock')
+  const [gameHistory, setGameHistory] = useState([])  // 게임 승부 결과 기록
+
   const handleClick = (value) => {
     setHand(value)
-    setOtherHand(random(2))
+    const nextOtherHand = random(2)
+    const nextHistoryItem = getResult(value,nextOtherHand)
+    setOtherHand(nextOtherHand)
+    setGameHistory([...gameHistory, nextHistoryItem])
   }
   const handleClearClick = () => {
     setHand('rock')
     setOtherHand('rock')
+    setGameHistory([])  // 빈 배열로 초기화
   }
 
   return (
     <>
       <Button onClick={handleClearClick}>{'처음부터'}</Button>
-      <p>{getResult(Hand, OtherHand)}</p>
       <div>
         <HandIcon value={Hand} />
         vs
         <HandIcon value={OtherHand} />
+      </div>
+      <div>
+        <p>
+          승부 기록 : {gameHistory.join(', ')}
+        </p>
       </div>
       <HandButton value="rock" onClick={handleClick} />
       <HandButton value="scissor" onClick={handleClick} />
